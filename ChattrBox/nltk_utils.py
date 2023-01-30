a1961793da441f1d1190e59f4454b55196f2bfd6
Rhythm Dutta <109751995+rhythmd18@users.noreply.github.com>
2023-01-30 21:49:04 +0530

    Updated nltk_utils to include comments

--- ChattrBox/nltk_utils.py
+++ ChattrBox/nltk_utils.py
@@ -17,7 +17,7 @@ def stem(word):
     print(stemmend_words)"""
     return stemmer.stem(word.lower())
 
-
+# Function to return a bag of words array
 def bag_of_words(tokenized_sentence, all_words):
     """
     return bag of words array:
@@ -27,11 +27,13 @@ def bag_of_words(tokenized_sentence, all_words):
     words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
     bag   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
     """
-    tokenized_sentence = [stem(w) for w in tokenized_sentence]
-    bag = np.zeros(len(all_words), dtype=np.float32)
+    tokenized_sentence = [stem(w) for w in tokenized_sentence] # Stemming the words in the tokenized sentence
+    bag = np.zeros(len(all_words), dtype=np.float32) # Creating a bag and initializing 0 for each word
+    # Iterating through all the words
     for idx, w in enumerate(all_words):
+        # Iterating through each of the word in the tokenized sentence
         if w in tokenized_sentence:
-            bag[idx] = 1.0
+            bag[idx] = 1.0 # Assigning 1 to the word that contains in the tokenized sentence
 
     return bag
 
