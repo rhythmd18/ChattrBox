1655cb23f6db73796d1114c49cf475bf82ce2183
Rhythm Dutta <109751995+rhythmd18@users.noreply.github.com>
2023-01-30 08:11:06 +0000

    Making changes in the nltk_utils file to functio

--- ChattrBox/nltk_utils.py
+++ ChattrBox/nltk_utils.py
@@ -18,7 +18,6 @@ def bag_of_words(tokenized_sentence, all_words):
     pass
 
 
-a = "How long does shipping take?"
-print(a)
-a = tokenize(a)
-print(a)
+words = ['organize', 'organizes', 'organizing']
+stemmend_words = [stem(w) for w in words]
+print(stemmend_words)
