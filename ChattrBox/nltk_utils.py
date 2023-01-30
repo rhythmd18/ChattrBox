7cc1eb48bb3467752e8e705a327d6b89e5d4f585
Rhythm Dutta <109751995+rhythmd18@users.noreply.github.com>
2023-01-30 08:42:19 +0000

    Necessary changes made

--- ChattrBox/nltk_utils.py
+++ ChattrBox/nltk_utils.py
@@ -1,4 +1,5 @@
 import nltk
+import numpy as np
 #nltk.download('punkt')
 
 # Importing a stemmer
@@ -11,13 +12,31 @@ def tokenize(sentence):
 
 # Function to implement the notion of stemming
 def stem(word):
+    """words = ['organize', 'organizes', 'organizing']
+    stemmend_words = [stem(w) for w in words]
+    print(stemmend_words)"""
     return stemmer.stem(word.lower())
 
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
+    """
+    return bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bag   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    """
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+
+    return bag
 
 
-words = ['organize', 'organizes', 'organizing']
-stemmend_words = [stem(w) for w in words]
-print(stemmend_words)
+sentence = ["hello", "how", "are", "you"]
+words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+bag = bag_of_words(sentence, words)
+print(bag)
---
+++ ChattrBox/train.py
@@ -0,0 +1,36 @@
+import json
+from nltk_utils import tokenize, stem, bag_of_words
+import numpy as np
+
+with open('intents.json', 'r') as f:
+    intents = json.load(f)
+
+all_words = []
+tags = []
+xy = []
+
+for intent in intents['intents']:
+    tag = intent['tag']
+    tags.append(tag)
+    for pattern in intent['patterns']:
+        w = tokenize(pattern)
+        all_words.extend(w)
+        xy.append((w, tag))
+
+ignore_words = ['?', '!', '.', ',']
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
+print(tags)
+
+x_train = []
+y_train = []
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
+    x_train.append(bag)
+
+    label = tags.index(tag)
+    y_train.append(label)
+
+x_train = np.array(x_train)
+y_train = np.array(y_train)
